ae3f5046bef117c4b922a50f24cf538fa8c747e3
SaradaPanigrahy <98700255+SaradaPanigrahy@users.noreply.github.com>
2025-09-29 20:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ // Get current URL
+    const currentURL = location.href;
+
+    // Find length of URL
+    const urlLength = currentURL.length;
+
+    // Show alert
+    alert("The length of the URL is: " + urlLength);
